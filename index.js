42cab40fade2dab1622cb6e7c8ea86006db866bb
azocher <anna.zocher@gmail.com>
2020-07-24 12:12:32 -0700

    User model added; part way through index work

---
+++ index.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const passport = require("passport")
+const bodyParser = require("body-parser")
+
+// initialize Express server
+const app = express()
+
+// require router
+//const users = require('./routes/api/users')
+
+// middleware to CORS requests
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+    next()
+})
+
+// bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// config our DB
+const db = process.env.MONGODB_URI
+
+// connect to Mongodb (using Atlas)
+mongoose.connect(db)
+    .then((() => console.log('MongoDB connected... 🥭')))
+    .catch(err => console.log(err))
+
+// test routing
+
+// passport middleware
+
+// passport JWT token set/config
+
+// setup our routes
+
+// start our server
+app.listen(process.env.PORT || 5000, () => console.log(`Server is running on ${process.env.PORT} and things are looking good 🤗`))
+
